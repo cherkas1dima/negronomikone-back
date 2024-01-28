f8f24bafe6a730c212dfde078589ac1ff1370941
cherkas1dima <cherkas231095@gmail.com>
2024-01-28 23:29:25 +0300

    Initial commit

---
+++ src/main/java/zmiter/i/co/negronomwall/NegronomWallApplication.java
@@ -0,0 +1,13 @@
+package zmiter.i.co.negronomwall;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NegronomWallApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NegronomWallApplication.class, args);
+    }
+
+}
---
+++ src/test/java/zmiter/i/co/negronomwall/NegronomWallApplicationTests.java
@@ -0,0 +1,13 @@
+package zmiter.i.co.negronomwall;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NegronomWallApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
